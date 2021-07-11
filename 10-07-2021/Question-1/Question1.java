eeaae35ee3838b6a45f6f0ed448d68143f8fa68b
Talha Yaseen <khantalhakhan94@gmail.com>
2021-07-11 10:44:20 +0530

    Question1: Methods Overridden and Overloading implementation with properly commented codes

--- 10-07-2021/Question-1/Question1.java
+++ 10-07-2021/Question-1/Question1.java
@@ -1,14 +1,20 @@
-// Overriding: if subclass (child class) has the same method as declared in the parent class
-// Overloading: if a class has multiple methods having same name but different in parameters
+/**
+ * 
+ * @author Talha Yaseen
+ * 
+ * 
+ * Overridden: if subclass (child class) has the same method as declared in the parent class
+ * Overloading: if a class has multiple methods having same name but different in parameters
+ */
 
 // parent class
 public class Question1 {
 	public void add(int a, int b) {
-		System.out.println((a+b) + " parent method 1");
+		System.out.printf("%d + %d = %d : parent method-1 executed%n", a, b, a+b);
 	}
 	
 	public void add(int a, int b, int c) {
-		System.out.println((a+b+c) + " parent method 2");
+		System.out.printf("%d + %d + %d = %d : parent method-2 executed%n", a, b, c, a+b+c);
 	}
 }
 
@@ -17,6 +23,6 @@ class Question1Child extends Question1 {
 	
 	@Override
 	public void add (int a, int b) {
-		System.out.println((a+b) + " child method");
+		System.out.printf("%d + %d = %d : child method executed%n", a, b, a+b);
 	}
 }
--- 10-07-2021/Question-1/Question1Test.java
+++ 10-07-2021/Question-1/Question1Test.java
@@ -1,16 +1,25 @@
-
+/**
+ * 
+ * @author Talha Yaseen
+ * 
+ * Overridden and Overloading test
+ */
 public class Question1Test {
 	public static void main(String[] args) {
 		
-		// overriding test
-		Question1Child objC = new Question1Child();
-		objC.add(2, 3);
+		/**
+		 *  overridden test
+		 */
+		Question1Child objChild = new Question1Child();
+		objChild.add(2, 3);
 		
 		
-		// overloading test
-		Question1 objP = new Question1();
-		objP.add(3, 6);
-		objP.add(2, 3, 5);
+		/**
+		 *  overloading test
+		 */
+		Question1 objParent = new Question1();
+		objParent.add(3, 6);
+		objParent.add(2, 3, 5);
 	}
 }
 
